40167fedf1551d06781139dd5d12a33bbdda866e
piyush.gyl <piyush2022ug@gmail.com>
2025-03-03 11:08:59 +0530

    feat: playground-023-backend (jwt auth)

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const app = express();
+
+const SECRET_KEY = "supersecretadmin";
+const JWT_SECRET = "your_jwt_secret";
+
+app.use(express.json());
+
+const verifyToken = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    return res.status(401).json({ message: "No Token Provided" });
+  }
+
+  try {
+    console.log(token);
+    const decodedToken = jwt.verify(token, JWT_SECRET);
+    req.user = decodedToken;
+    next();
+  } catch (error) {
+    return res.status(402).json({ message: "Invalid token" });
+  }
+};
+
+app.post("/admin/login", (req, res) => {
+  const { secret } = req.body;
+
+  if (secret === SECRET_KEY) {
+    const token = jwt.sign(
+      {
+        role: "admin",
+      },
+      JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+    res.json({ token });
+  } else {
+    res.json({ message: "Invalid secret" });
+  }
+});
+
+app.get("/admin/api/data", verifyToken, (req, res) => {
+  res.json({ message: "Protected route accessible" });
+});
+
+app.listen(3000, () => console.log("Server is running on 3000"));
